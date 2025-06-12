f1a9872ab35d071b677b3005c310204168ed41f6
Gabriel Dias <gabrielbertodias@hotmail.com>
2025-06-12 18:45:24 +0000

    configuração para Firebird e instruções atualizadas

--- config/config.php
+++ config/config.php
@@ -2,5 +2,12 @@
 // Configurações do sistema
 
 return [
+    'db' => [
+        'driver' => 'firebird',
+        'host' => 'localhost',
+        'database' => '/var/lib/firebird/2.5/data/gabinete.gdb',
+        'username' => 'SYSDBA',
+        'password' => 'interpay',
+    ],
     'app_name' => 'Meu Sistema PHP',
 ];
